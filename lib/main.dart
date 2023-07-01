3e0be79cda60ef7192f548b401daba3aad60b440
Grzan <grzegorz.niespodzianek@gmail.com>
2023-07-01 14:19:27 +0200

    initial

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: ElevatedButton(
+              onPressed: () {},
+              child: const Text("I want to receive notification")),
+        ),
+      ),
+    );
+  }
+}
